089f17fe9ae783b8c45bbb0fa08372762e16548b
sanyaswb <sanyaswb@gmail.com>
2022-10-11 11:40:09 +0300

    update redmy

--- hw_02_2/python/main.py
+++ hw_02_2/python/main.py
@@ -1,1 +1,21 @@
-print('Hello, World!')
+a = int(input())
+b = int(input())
+
+c = a + b
+print('add:', c)
+
+c = a - b
+print('subtract:', c)
+
+c = a * b
+print('multiply:', c)
+
+c = a / b
+print('divide:', c)
+
+c = a // b
+print('rounding:', c)
+
+c = a % b
+print('remainder:', c)
+
